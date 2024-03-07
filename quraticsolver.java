b512ba0ac8d9649fc857884f8d2d11cbbe112ac2
MatsoA <19639672+MatsoA@users.noreply.github.com>
2024-03-07 14:38:22 -0500

    trigger jenkins

--- quraticsolver.java
+++ quraticsolver.java
@@ -4,7 +4,7 @@ public class quraticsolver {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		int a = 3;
-		int b = 5;
+		int b = 6;
 		int c = 10;
 		if (b*b - 4*a*c <0)
 			System.out.println("This equation " + a + "x^2 + " + b +"x + " + c +" has no solution");
